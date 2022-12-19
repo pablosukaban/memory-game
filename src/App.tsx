45aae65cc44995a43a0ce0befdaf47057141f878
ilya <zapaska96@gmail.com>
2022-12-19 20:39:32 +0500

    светлая тема

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,9 @@ type Cell = {
 export const App = () => {
     const [mainArray, setMainArray] = useState<Cell[]>(getRandomArray);
     const [choiscesArray, setChoicesArray] = useState<Cell[]>([]);
+    const [isLightTheme, setIsLightTheme] = useState(false);
 
     const isAllCorrect = mainArray.every((item) => item.isCorrect);
-
-    if (isAllCorrect) {
-        console.log('win');
-    }
 
     const hideAllCells = () => {
         const allClearArray = mainArray.map((item) => ({
@@ -51,6 +48,7 @@ export const App = () => {
 
         if (!clickedCell) return;
         if (choiscesArray.length === 2) return;
+
         setChoicesArray((prev) => [...prev, clickedCell]);
 
         const clickedArr = mainArray.map((item) => {
@@ -59,11 +57,23 @@ export const App = () => {
             }
             return item;
         });
+
         setMainArray(clickedArr);
     };
 
     const handleRestartClick = () => {
         setMainArray(getRandomArray);
+    };
+
+    const handleThemeClick = () => {
+        const doc = document.documentElement;
+        doc.classList.toggle('light-theme');
+
+        if (document.documentElement.className.includes('light-theme')) {
+            setIsLightTheme(true);
+        } else {
+            setIsLightTheme(false);
+        }
     };
 
     useEffect(() => {
@@ -74,7 +84,53 @@ export const App = () => {
 
     return (
         <div className='container'>
-            {isAllCorrect ? <h1 className='board-title'>Победа!</h1> : null}
+            <button className='theme-btn' onClick={handleThemeClick}>
+                {isLightTheme ? (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='28'
+                        height='28'
+                        viewBox='0 0 28 28'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='feather feather-moon'
+                    >
+                        <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='28'
+                        height='28'
+                        viewBox='0 0 28 28'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='feather feather-sun'
+                    >
+                        <circle cx='12' cy='12' r='5'></circle>
+                        <line x1='12' y1='1' x2='12' y2='3'></line>
+                        <line x1='12' y1='21' x2='12' y2='23'></line>
+                        <line x1='4.22' y1='4.22' x2='5.64' y2='5.64'></line>
+                        <line
+                            x1='18.36'
+                            y1='18.36'
+                            x2='19.78'
+                            y2='19.78'
+                        ></line>
+                        <line x1='1' y1='12' x2='3' y2='12'></line>
+                        <line x1='21' y1='12' x2='23' y2='12'></line>
+                        <line x1='4.22' y1='19.78' x2='5.64' y2='18.36'></line>
+                        <line x1='18.36' y1='5.64' x2='19.78' y2='4.22'></line>
+                    </svg>
+                )}
+            </button>
+            <h1 className='board-title'>{isAllCorrect ? 'Победа!' : ''}</h1>
             <div className='board-grid'>
                 {mainArray.map((item) => (
                     <button
@@ -92,11 +148,9 @@ export const App = () => {
                     </button>
                 ))}
             </div>
-            {isAllCorrect ? (
-                <button onClick={handleRestartClick} className='restart-btn'>
-                    Еще
-                </button>
-            ) : null}
+            <button onClick={handleRestartClick} className='restart-btn'>
+                {isAllCorrect ? 'Еще раз' : ''}
+            </button>
         </div>
     );
 };
@@ -119,13 +173,10 @@ function shuffle(array: Cell[]) {
     let currentIndex = array.length,
         randomIndex;
 
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex],
             array[currentIndex],
