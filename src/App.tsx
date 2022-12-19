564ce3ea951d9056a017b7c6f24a4bce8fe809bf
ilya <zapaska96@gmail.com>
2022-12-19 20:52:31 +0500

    кнопку в отдельный файл

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Grid } from './Grid';
+import { ThemeButton } from './ThemeButton';
 import { getRandomArray } from './utils';
 
 // TODO таймер, сложность
@@ -15,7 +16,6 @@ export type Cell = {
 export const App = () => {
     const [mainArray, setMainArray] = useState<Cell[]>(getRandomArray);
     const [choiscesArray, setChoicesArray] = useState<Cell[]>([]);
-    const [isLightTheme, setIsLightTheme] = useState(false);
 
     const hideAllCells = () => {
         const allClearArray = mainArray.map((item) => ({
@@ -64,17 +64,6 @@ export const App = () => {
         setMainArray(getRandomArray);
     };
 
-    const handleThemeClick = () => {
-        const doc = document.documentElement;
-        doc.classList.toggle('light-theme');
-
-        if (document.documentElement.className.includes('light-theme')) {
-            setIsLightTheme(true);
-        } else {
-            setIsLightTheme(false);
-        }
-    };
-
     useEffect(() => {
         if (choiscesArray.length === 2) {
             setTimeout(moveOver, 1500);
@@ -83,52 +72,7 @@ export const App = () => {
 
     return (
         <div className='container'>
-            <button className='theme-btn' onClick={handleThemeClick}>
-                {isLightTheme ? (
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='28'
-                        height='28'
-                        viewBox='0 0 28 28'
-                        fill='none'
-                        stroke='currentColor'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        className='feather feather-moon'
-                    >
-                        <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
-                    </svg>
-                ) : (
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='28'
-                        height='28'
-                        viewBox='0 0 28 28'
-                        fill='none'
-                        stroke='currentColor'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        className='feather feather-sun'
-                    >
-                        <circle cx='12' cy='12' r='5'></circle>
-                        <line x1='12' y1='1' x2='12' y2='3'></line>
-                        <line x1='12' y1='21' x2='12' y2='23'></line>
-                        <line x1='4.22' y1='4.22' x2='5.64' y2='5.64'></line>
-                        <line
-                            x1='18.36'
-                            y1='18.36'
-                            x2='19.78'
-                            y2='19.78'
-                        ></line>
-                        <line x1='1' y1='12' x2='3' y2='12'></line>
-                        <line x1='21' y1='12' x2='23' y2='12'></line>
-                        <line x1='4.22' y1='19.78' x2='5.64' y2='18.36'></line>
-                        <line x1='18.36' y1='5.64' x2='19.78' y2='4.22'></line>
-                    </svg>
-                )}
-            </button>
+            <ThemeButton />
             <Grid
                 handleCellClick={handleCellClick}
                 mainArray={mainArray}
